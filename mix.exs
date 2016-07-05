b9e6e12522288c75695c4054620be100b6d3ab5a
Davide Aversa <thek3nger@gmail.com>
2016-07-05 10:17:48 +0200

    Added package info in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule DarianCalendar.Mixfile do
     [app: :darian_calendar,
      version: "0.1.0",
      elixir: "~> 1.3",
+     description: "Package to convert Terrestrian date into Martian date according the Darian Calendar.",
+     package: package(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -28,5 +30,13 @@ defmodule DarianCalendar.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp package do
+    [# These are the default files included in the package
+     files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+     maintainers: ["Davide Aversa"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/THeK3nger/darian-calendar-elixir"}]
   end
 end
